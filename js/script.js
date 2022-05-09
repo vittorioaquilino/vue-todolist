20d3118327aa06d3096aabd28ca0b346f7eb4481
vittorioaquilino <vittorioaquilino17@gmail.com>
2022-05-09 16:54:34 +0200

    add bonus 1

--- js/script.js
+++ js/script.js
@@ -4,16 +4,20 @@
 // - text, una stringa che indica il testo del todo
 // - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
 
-// MILESTONE 1
+// MILESTONE 1 [completato]
 // Stampare all'interno di una lista, un item per ogni todo. [completato]
 // Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato. [completato]
 
 // MILESTONE 2 [completato]
 // Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista. [completato]
 
-// MILESTONE 3
+// MILESTONE 3 [completato]
 // Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, 
 // il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti. [completato]
+
+// Bonus:
+// 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista [completato]
+// 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa).
 
 const app = new Vue(
     {
@@ -48,12 +52,16 @@ const app = new Vue(
             addToDo: function() {
                 
                 // creo nuovo todo
-                const newToDo = {
-                    text: this.newToDo,
-                    done: false,
+                if(this.newToDo !== '') {
+                    const newToDo = {
+                        text: this.newToDo,
+                        done: false,
+                    }
+                    this.todos.push(newToDo);
+                    this.newToDo = "";
+                } else {
+                    alert("devi scrivere un'azione da svolgere ")
                 }
-                this.todos.push(newToDo);
-                this.newToDo = "";
                 
             },
 
@@ -71,8 +79,6 @@ const app = new Vue(
                     clicked.done = true;
                 }
             },
-
-
         }
     }
 )
